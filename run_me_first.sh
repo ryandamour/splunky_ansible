5b3d9b020e2d76ba1024a4054df38c8ddd59e066
ryandamour <ryandamour@gmail.com>
2018-06-28 22:14:23 -0700

    Ansible Installation For Splunk Install

---
+++ run_me_first.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O roles/base/files/splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.1&product=splunk&filename=splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm&wget=true'
